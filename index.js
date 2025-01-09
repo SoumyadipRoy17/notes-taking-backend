6361ddf653d72ec9ee6381eddbb86a2eafa411c3
Soumyadip Roy <soumyaroy172003@gmail.com>
2025-01-09 06:46:35 +0530

    Added an edit feature with fs rename module

--- index.js
+++ index.js
@@ -34,6 +34,34 @@ app.get("/file/:filename", (req, res) => {
   });
 });
 
+app.post("/edit", (req, res) => {
+  fs.rename(
+    `./files/${req.body.previous}`,
+    `./files/${req.body.new}`,
+    (err) => {
+      if (err) {
+        console.log(err);
+      }
+      fs.writeFile(`./files/${req.body.new}`, req.body.newDetails, (err) => {
+        if (err) {
+          console.log(err);
+        }
+        res.redirect("/");
+      });
+    }
+  );
+});
+
+app.get("/edit/:filename", (req, res) => {
+  fs.readFile(`./files/${req.params.filename}`, "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(data);
+    res.render("edit", { data: data, filename: req.params.filename });
+  });
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
